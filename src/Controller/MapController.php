02c4507f7651cbdea5b52a3194c65979d1ddd25e
Olivier RATIEVILLE <olivier.ratieville@gmail.com>
2026-02-24 18:52:35 +0100

    Try map markers custom icons

--- src/Controller/MapController.php
+++ src/Controller/MapController.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\UX\Map\Bridge\Leaflet\LeafletOptions;
 use Symfony\UX\Map\Bridge\Leaflet\Option\TileLayer;
+use Symfony\UX\Map\Icon\Icon;
 use Symfony\UX\Map\InfoWindow;
 use Symfony\UX\Map\Map;
 use Symfony\UX\Map\Marker;
@@ -28,11 +29,7 @@ final class MapController extends AbstractController
                 ]);
             }
         }
-        /*
-        $icon = MapIcon::url('/icons/bars.png')
-        ->size(40, 40)
-        ->anchor(20, 40);
-*/
+        
         $map = (new Map('default'))
             ->center(new Point(49.433331, 1.08333))
             ->zoom(8)
@@ -61,15 +58,33 @@ final class MapController extends AbstractController
             )
         ));
 
-        $bars = $overpass->getInArea('Rouen', 'bars');
+        /*
+        $icon = Icon::url ('/icons/bars.png');
+        */
+/*
+        $iconBars = $map.Icon({ options: '/icons/bars.png'  });
+*/
+
+        $iconBars = new Icon()->url('/icons/beer.png');
+
+        $iconBars = Icon::url('/icons/beer.png');
+        /*
+            ->size(40, 40)
+            ->anchor(20, 40);
+*/
+         $bars = $overpass->getInArea('Rouen', 'bars');
         foreach ($bars as $bar) {
-            $map->addMarker(new Marker(
+            $marker = new Marker(
                 position: new Point($bar['lat'], $bar['lon']),
                 title: $bar['name'],
                 infoWindow: new InfoWindow(
                     content: '<p>' . $bar['name'] . '<br>' . $bar['address'] . '<form data-turbo-frame="local_data" method="post"><input type="hidden" name="idElement" value="Rouen"><input type="submit" value="Infos"></form></p>',
                 )
-            ));
+            );
+
+            $marker->icon();
+
+            $map->addMarker( $marker);
         }
 
         $hotels = $overpass->getInArea('Rouen', 'hotels');
