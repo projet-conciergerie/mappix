2e5a0a1be64fdda5ecf50cfe3f06896ddbc2ec8e
Marliva <cedriclevasseur76@gmail.com>
2026-02-24 15:28:05 +0100

    modifs cruds précédents

--- src/Controller/Tourisme/LocalisationCrudController.php
+++ src/Controller/Tourisme/LocalisationCrudController.php
@@ -4,9 +4,7 @@ namespace App\Controller\Tourisme;
 
 use App\Entity\Localisation;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class LocalisationCrudController extends AbstractCrudController
