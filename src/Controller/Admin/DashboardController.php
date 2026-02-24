deb81091fad334a127153c321f4ac61ae59d860e
totorz76 <victor76.zammit@gmail.com>
2026-02-24 16:44:54 +0100

    UserCrudController

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -15,8 +15,10 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[AdminDashboard(routePath: '/admin', routeName: 'admin')]
+#[IsGranted('ROLE_ADMIN')]
 class DashboardController extends AbstractDashboardController
 {
     public function index(): Response
@@ -56,7 +58,7 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::linkToCrud('Service', 'fas fa-list', Service::class);
         yield MenuItem::linkToCrud('User', 'fas fa-list', User::class);
         yield MenuItem::linkToCrud('Reservation', 'fas fa-list', Reservation::class);
-        yield MenuItem::linkToCrud('Localisation', 'fas fa-list', Localisation::class);
+        // yield MenuItem::linkToCrud('Localisation', 'fas fa-list', Localisation::class);
         yield MenuItem::linkToCrud('Evenement', 'fas fa-list', Evenement::class);
         yield MenuItem::linkToCrud('Contact', 'fas fa-list', Contact::class);
         yield MenuItem::linkToCrud('Category', 'fas fa-list', Category::class);
--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -3,12 +3,17 @@
 namespace App\Controller\Admin;
 
 use App\Entity\User;
+use Doctrine\Inflector\Language;
+use Doctrine\ORM\EntityManagerInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 
 class UserCrudController extends AbstractCrudController
 {
@@ -17,20 +22,53 @@ class UserCrudController extends AbstractCrudController
     {
         return User::class;
     }
+    public function persistEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        if (!$entityInstance instanceof User) return;
+
+        if (!in_array('ROLE_ADMIN', $entityInstance->getRoles())) {
+            throw new \Exception('Un administrateur doit garder au moins un ROLE_ADMIN.');
+        }
+
+        parent::persistEntity($entityManager, $entityInstance);
+    }
 
     public function configureActions(Actions $actions): Actions
     {
         return $actions
-            ->disable(Action::NEW)      // désactive création
-            ->disable(Action::EDIT);     // désactive modification
-            // ->disable(Action::DELETE);  // optionnel
+            ->disable(Action::NEW);      // désactive création
+        // ->disable(Action::EDIT);     // désactive modification
+        // ->disable(Action::DELETE);  // optionnel
     }
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->hideOnForm(),
+            TextField::new('email')->hideOnForm(),
+            ArrayField::new('roles')->hideOnForm(),
+            TextField::new('nom')->hideOnForm(),
+            TextField::new('prenom')->hideOnForm(),
+            TextField::new('pseudo')->hideOnForm(),
+            TextField::new('langue')->hideOnForm(),
+            DateTimeField::new('lastConnectedAt')->hideOnForm(),
+            ChoiceField::new('roles')
+                ->setChoices([
+                    'Utilisateur' => 'ROLE_USER',
+                    'Administrateur' => 'ROLE_ADMIN',
+                ])
+                ->allowMultipleChoices()
+                ->renderExpanded(true)
+                ->onlyOnForms() // n’apparaît que dans le formulaire d’édition
         ];
+    }
+    public function updateEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        if (!$entityInstance instanceof User) return;
+
+        // On force à ne mettre à jour que les rôles
+        $userFromDb = $entityManager->getRepository(User::class)->find($entityInstance->getId());
+        $userFromDb->setRoles($entityInstance->getRoles());
+
+        $entityManager->flush();
     }
 }
