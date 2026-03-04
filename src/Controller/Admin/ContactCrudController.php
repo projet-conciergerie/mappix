a507b24798c7b03daa58bac68dc52711a2e6be12
totorz76 <victor76.zammit@gmail.com>
2026-03-04 10:51:30 +0100

    oui

--- src/Controller/Admin/ContactCrudController.php
+++ src/Controller/Admin/ContactCrudController.php
@@ -3,9 +3,11 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Contact;
+use Dom\Text;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
@@ -19,8 +21,8 @@ class ContactCrudController extends AbstractCrudController
     public function configureActions(Actions $actions): Actions
     {
         return $actions
-        ->disable(Action::NEW)    // désactive création
-        ->disable(Action::EDIT);   // désactive modification
+        ->disable(Action::NEW);    // désactive création
+        // ->disable(Action::EDIT);   // désactive modification
         // ->disable(Action::DELETE);  // optionnel
     }
 
@@ -28,9 +30,14 @@ class ContactCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('email'),
-            TextField::new('message'),
+            IdField::new('id')->hideOnForm(),
+            TextField::new('email')->hideOnForm(),
+            TextField::new('message')->hideOnForm(),
+             ChoiceField::new('type')
+                ->setChoices([
+                    'Commentaire' => 'commentaire',
+                    'Réclamation' => 'reclamation',
+                ])
         ];
     }
 
