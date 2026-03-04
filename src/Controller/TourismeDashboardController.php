08b0c0db67b00ecfc4943600085bf6037a7f0494
Marliva <cedriclevasseur76@gmail.com>
2026-03-04 15:37:24 +0100

    Test d'ajout tailwind dashboard tourisme

--- src/Controller/TourismeDashboardController.php
+++ src/Controller/TourismeDashboardController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use App\Entity\Category;
 use App\Entity\Evenement;
-use App\Entity\Localisation;
 use App\Entity\Reservation;
 use App\Entity\Service;
 use App\Entity\User;
@@ -25,18 +24,23 @@ class TourismeDashboardController extends AbstractDashboardController
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('Office de Tourisme');
+            ->setTitle('Office de Tourisme - Rouen');
     }
 
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Accueil', 'fa fa-home');
+        
+        yield MenuItem::section('Gestion');
         yield MenuItem::linkToCrud('Utilisateurs', 'fas fa-users', User::class);
-        yield MenuItem::linkToCrud('Catégories', 'fas fa-list', Category::class);
-        yield MenuItem::linkToCrud('Evénements', 'fas fa-list', Evenement::class);
-        yield MenuItem::linkToCrud('Reservations', 'fas fa-list', Reservation::class);
-        yield MenuItem::linkToCrud('Services', 'fas fa-list', Service::class);
-
-        // yield MenuItem::linkToCrud('Localisations', 'fas fa-list', Localisation::class);
+        yield MenuItem::linkToCrud('Réservations', 'fas fa-calendar-check', Reservation::class);
+        
+        yield MenuItem::section('Contenu');
+        yield MenuItem::linkToCrud('Événements', 'fas fa-calendar-alt', Evenement::class);
+        yield MenuItem::linkToCrud('Services', 'fas fa-concierge-bell', Service::class);
+        yield MenuItem::linkToCrud('Catégories', 'fas fa-folder', Category::class);
+        
+        yield MenuItem::section('Navigation');
+        yield MenuItem::linkToRoute('Retour au site', 'fas fa-arrow-left', 'app_home');
     }
 }
